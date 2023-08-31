1690f7e3a1704b2a0c1f3e5db9c1f3d91e1d4473
o7Moon <0167moon@gmail.com>
2023-08-31 16:18:59 -0500

    move packet handlers

--- src/main/java/dev/o7moon/openboatutils/ClientboundPackets.java
+++ src/main/java/dev/o7moon/openboatutils/ClientboundPackets.java
@@ -1,5 +1,9 @@
 package dev.o7moon.openboatutils;
 
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+
+import java.util.Arrays;
+
 public enum ClientboundPackets {
     RESET,
     SET_STEP_HEIGHT,
@@ -9,5 +13,57 @@ public enum ClientboundPackets {
     SET_BOAT_WATER_ELEVATION,
     SET_AIR_CONTROL,
     SET_BOAT_JUMP_FORCE,
-    SET_MODE,
+    SET_MODE;
+
+    public static void registerHandlers(){
+        ClientPlayNetworking.registerGlobalReceiver(OpenBoatUtils.settingsChannel, (client, handler, buf, responseSender) -> {
+            try {
+                short packetID = buf.readShort();
+                switch (packetID) {
+                    case 0:
+                        OpenBoatUtils.resetSettings();
+                        return;
+                    case 1:
+                        float stepSize = buf.readFloat();
+                        OpenBoatUtils.setStepSize(stepSize);
+                        return;
+                    case 2:
+                        float slipperiness = buf.readFloat();
+                        OpenBoatUtils.setAllBlocksSlipperiness(slipperiness);
+                        return;
+                    case 3:
+                        slipperiness = buf.readFloat();
+                        String blocks = buf.readString();
+                        String[] blocksArray = blocks.split(",");
+                        OpenBoatUtils.setBlocksSlipperiness(Arrays.asList(blocksArray), slipperiness);
+                        return;
+                    case 4:
+                        boolean fallDamage = buf.readBoolean();
+                        OpenBoatUtils.setFallDamage(fallDamage);
+                        return;
+                    case 5:
+                        boolean waterElevation = buf.readBoolean();
+                        OpenBoatUtils.setWaterElevation(waterElevation);
+                        return;
+                    case 6:
+                        boolean airControl = buf.readBoolean();
+                        OpenBoatUtils.setAirControl(airControl);
+                        return;
+                    case 7:
+                        float jumpForce = buf.readFloat();
+                        OpenBoatUtils.setJumpForce(jumpForce);
+                        return;
+                    case 8:
+                        short mode = buf.readShort();
+                        Modes.setMode(Modes.values()[mode]);
+                        return;
+                }
+            } catch (Exception E) {
+                OpenBoatUtils.LOG.error("Error when handling clientbound openboatutils packet: ");
+                for (StackTraceElement e : E.getStackTrace()){
+                    OpenBoatUtils.LOG.error(e.toString());
+                }
+            }
+        });
+    }
 }
--- src/main/java/dev/o7moon/openboatutils/ServerboundPackets.java
+++ src/main/java/dev/o7moon/openboatutils/ServerboundPackets.java
@@ -1,5 +1,26 @@
 package dev.o7moon.openboatutils;
 
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+
 public enum ServerboundPackets {
-    VERSION,
+    VERSION;
+
+    public static void registerHandlers(){
+        ServerPlayNetworking.registerGlobalReceiver(OpenBoatUtils.settingsChannel, (server, player, handler, buf, responseSender) -> {
+            try {
+                short packetID = buf.readShort();
+                switch (packetID) {
+                    case 0:
+                        int versionID = buf.readInt();
+                        OpenBoatUtils.LOG.info("OpenBoatUtils version received by server: "+versionID);
+                        return;
+                }
+            } catch (Exception E) {
+                OpenBoatUtils.LOG.error("Error when handling serverbound openboatutils packet: ");
+                for (StackTraceElement e : E.getStackTrace()){
+                    OpenBoatUtils.LOG.error(e.toString());
+                }
+            }
+        });
+    }
 }
