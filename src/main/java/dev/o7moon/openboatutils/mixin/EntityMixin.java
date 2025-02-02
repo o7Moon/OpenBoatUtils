f3fca43c9159bd5172b67d7619c342f91c072645
o7Moon <0167moon@gmail.com>
2025-02-02 13:27:09 -0600

    1.21.3 stepping hotfix

--- src/main/java/dev/o7moon/openboatutils/mixin/EntityMixin.java
+++ src/main/java/dev/o7moon/openboatutils/mixin/EntityMixin.java
@@ -3,6 +3,9 @@ package dev.o7moon.openboatutils.mixin;
 import dev.o7moon.openboatutils.GetStepHeight;
 import dev.o7moon.openboatutils.OpenBoatUtils;
 import net.minecraft.entity.Entity;
+//? >=1.21.3 {
+/*import net.minecraft.entity.vehicle.AbstractBoatEntity;
+*///?}
 import net.minecraft.entity.vehicle.BoatEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +19,12 @@ public abstract class EntityMixin {
     //? >=1.21 {
     /*@Inject(method = "getStepHeight", at = @At("HEAD"), cancellable = true)
     public void getStepHeight(CallbackInfoReturnable<Float> cir) {
-        if ((Object) this instanceof BoatEntity) {
+        //? >=1.21.3 {
+        if ((Object) this instanceof AbstractBoatEntity) {
+        //?}
+        //? <=1.21 {
+        /^if ((Object) this instanceof BoatEntity) {
+        ^///?}
             GetStepHeight step = (GetStepHeight) this;
             cir.setReturnValue(step.getStepHeight());
             cir.cancel();
@@ -26,7 +34,12 @@ public abstract class EntityMixin {
 
     @ModifyVariable(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;", at = @At("STORE"), ordinal = 3)
     private boolean hookStepHeightOnGroundCheck(boolean original) {
+        //? <=1.21 {
         if ((Object) this instanceof BoatEntity) {
+        //?}
+        //? >=1.21.3 {
+        /*if ((Object) this instanceof AbstractBoatEntity) {
+        *///?}
             if (OpenBoatUtils.canStepWhileFalling()) {
                 return true;
             }
